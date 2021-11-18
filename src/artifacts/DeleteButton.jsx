543a4c64c0e164122704f105e4a40dc33774ab8f
Jesse Beard <61173820+jessebeard@users.noreply.github.com>
2021-11-18 02:07:39 -0800

    just some code cleanliness

--- src/artifacts/DeleteButton.jsx
+++ src/artifacts/DeleteButton.jsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react';
 const DeleteButton = ({rowId, action}) => {
   const [toDelete, setToDelete] = useState(false);
 
-
   const handleDelete = () => {
     setToDelete(true);
   };
